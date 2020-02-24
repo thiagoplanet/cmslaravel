abfefef927129c2250152d10dfc153f8bca70fb3
Thiago Planet <thiagoplanet2004@hotmail.com>
2020-02-24 11:34:47 -0300

    comit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,11 +13,12 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: red;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
+                
             }
 
             .full-height {
