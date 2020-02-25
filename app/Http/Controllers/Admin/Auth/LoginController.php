4cbad39aa889b5b6a940f08e93b6ea8e7ffe7b96
Thiago Planet <thiagoplanet2004@hotmail.com>
2020-02-25 10:02:38 -0300

    Criando rotas de Login para o admin, também saida pelo controller de login

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Admin\Auth; //mudamos o nome pq está em outra pasta
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/painel';
 
     /**
      * Create a new controller instance.
@@ -36,5 +36,9 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    
+    public function index(){
+        echo 'tela de login';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ Route::get('/','Site\HomeController@index');
 
 
 Route::prefix('painel')->group(function () {
-    Route::get('/','Admin\HomeController@index');
+    Route::get('/','Admin\HomeController@index')->name('admin');
+    Route::get('login', 'Admin\Auth\LoginController@index')->name('login');
 });
 
 
